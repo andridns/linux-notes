199271f0543dc2cfc16c2a8bbb94d67ccad78115
Andri Danusasmita <andridsasmita@gmail.com>
2018-09-08 23:41:16 +0900

    wc script

---
+++ wc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Using wc (word count), find out how many lines, words
+# and characters there are in all the files in /var/log
+# that have the .log extension.
+# Click the link below to view a solution to the Lab exercise.
+# 
+# SOLUTION
+# $ wc /var/log/*.log
+#
+
+
