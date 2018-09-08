74284780494dc1dc02b6a80335496265d6a37c86
Andri Danusasmita <andridsasmita@gmail.com>
2018-09-08 23:38:10 +0900

    Add tee script 1

---
+++ tee1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# The tee utility is very useful for saving a copy of your output
+# while you are watching it being generated.
+# Execute a command such as doing a directory listing of the /etc directory:
+# student:/tmp> ls -l /etc
+# while both saving the output in a file and displaying it at your terminal.
+#
+# SOLUTION
+# $ ls -l /etc | tee /tmp/ls-output
+# $ less /tmp/ls-output
+#
+
